ce073f821f0e8b9370af24485c8a478bbdbf5c53
Felipe Salviano <felipesalviano00@hotmail.com>
2024-08-13 15:31:20 -0300

    teste commit

---
+++ js/responsive.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerMenu = document.getElementById('hamburgerMenu');
+    const navButtons = document.querySelector('.navButtons');
+
+    hamburgerMenu.addEventListener('click', () => {
+        navButtons.classList.toggle('mobile');
+        hamburgerMenu.classList.toggle('open');
+    });
+});
