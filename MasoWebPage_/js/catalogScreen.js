2595dba17dc3746f57856ac3d93d85b38f48423b
Felipe Salviano <felipesalviano00@hotmail.com>
2024-08-26 16:05:39 -0300

    atualização css, pagina de cadastro, atualização no port mobile

--- MasoWebPage_/js/catalogScreen.js
+++ MasoWebPage_/js/catalogScreen.js
@@ -6,6 +6,20 @@ document.addEventListener ('DOMContentLoaded', () => {
     const searchInput = document.getElementById('searchInput');
     const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
 
+    function filterAndSearch(collection, searchTerm) {
+        const catalogItems = document.querySelectorAll('.catalog-item');
+        catalogItems.forEach(item => {
+            const itemCollection = item.dataset.collection;
+            const itemName = item.dataset.name.toLowerCase();
+            
+            if ((collection === 'all' || itemCollection === collection) && itemName.includes(searchTerm)) {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
+        });
+    }
+
 // Função de filtragem
 filterSelect.addEventListener('change', () => {
     const collection = filterSelect.value;
--- MasoWebPage_/js/responsive.js
+++ MasoWebPage_/js/responsive.js
@@ -1,10 +1,18 @@
 /*document.addEventListener('DOMContentLoaded', () => {
-    const hamburgerMenu = document.getElementById('hamburgerMenu');
+    const hamburgerCheckbox = document.getElementById('label-check');
     const navButtons = document.querySelector('.navButtons');
 
-    hamburgerMenu.addEventListener('click', () => {
-        navButtons.classList.toggle('mobile');
-        hamburgerMenu.classList.toggle('open');
+    hamburgerCheckbox.addEventListener('change', () => {
+        if (hamburgerCheckbox.checked) {
+            navButtons.style.display = 'flex';  // Exibe os botões de navegação
+        } else {
+            navButtons.style.display = 'none';  // Esconde os botões de navegação
+        }
     });
-});
-*/
+
+    // Esconde os botões de navegação por padrão em dispositivos móveis
+    if (window.innerWidth <= 768) {
+        navButtons.style.display = 'none';
+    }
+});*/
+
