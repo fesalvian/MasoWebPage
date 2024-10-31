aad072f869187bdae1810844fa15a3b44f1dca89
Felipe Salviano <felipesalviano00@hotmail.com>
2024-10-31 20:50:50 -0300

    teste ainda 02

--- MasoWebPage_/js/login.js
+++ MasoWebPage_/js/login.js
@@ -1,8 +1,31 @@
-document.getElementById('loginForm').addEventListener('sumbit', async (e) => {
-    e.preventDefault(); //evita sumbit padrao do form
+document.getElementById('loginForm').addEventListener('sumbit', async (evento) => {
+    evento.preventDefault(); //evita sumbit padrao do form
+    console.log('botao funcionou nessa bosta');
+    
 
     const loginData = {
         login: document.getElementById('login').value, 
         senha: document.getElementById('senha').value,
+    };
+
+    try{
+        const response = await fetch ('http://localhost:8080/adm/login',{
+            method:'POST',
+            headers:{'Content-type':'application/json'},
+            body:JSON.stringify(loginData)
+
+        });
+
+        if (response.ok) {
+            const tokenDTO = await response.json();
+            const token = tokenDTO.token;
+            alert('Login bem-sucedido!');
+            localStorage.setItem('token', token); // Armazena o token para uso posterior
+        } else {
+            alert('Falha na autenticação.');
+        }
+    }catch (error){
+    console.error('Erro:', error);
+    alert('Erro no processo de login');
     }
-})
+});
